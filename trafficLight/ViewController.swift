741f7aa9c1100f0c3e272b7b026b2488260e81df
Kirill <133357229+kirillussr@users.noreply.github.com>
2023-05-31 10:02:16 +0300

    Lesson 2. My traffic light

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var trafficLightColorRed: UIView!
+    @IBOutlet var trafficLightColorYellow: UIView!
+    @IBOutlet var trafficLightColorGreen: UIView!
+    @IBOutlet var trafficLightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        trafficLightColorRed.layer.cornerRadius = 85
+        trafficLightColorYellow.layer.cornerRadius = 85
+        trafficLightColorGreen.layer.cornerRadius = 85
+        trafficLightButton.layer.cornerRadius = 20
+        
+        trafficLightColorRed.alpha = 0.3
+        trafficLightColorYellow.alpha = 0.3
+        trafficLightColorGreen.alpha = 0.3
+        
     }
-
-
+    
+    @IBAction func trafficLightButtonDidTapped() {
+        trafficLightButton.setTitle("NEXT", for: .normal)
+        
+        
+        if trafficLightColorRed.alpha == 1 {
+            trafficLightColorYellow.alpha = 1
+            trafficLightColorRed.alpha = 0.3
+        } else  if trafficLightColorYellow.alpha == 1 {
+            trafficLightColorGreen.alpha = 1
+            trafficLightColorYellow.alpha = 0.3
+        } else {
+            trafficLightColorGreen.alpha = 0.3
+            trafficLightColorRed.alpha = 1
+        }
+        
+    }
 }
-
